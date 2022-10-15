f2292800de6e57acdf02e061183bae182fed9df8
etseq97 <ugalaw97@gmail.com>
2022-10-15 00:27:23 -0400

    Create aliases.zsh

---
+++ aliases.zsh
@@ -0,0 +1,10 @@
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+alias up="sudo apt update && sudo apt -y upgrade"
+alias lsa="ls -a"
+alias upg="sudo apt upgrade"
+alias upt="sudo apt update"
+alias ins="sudo apt install"
+alias rem="sudo apt remove"
+alias pur="sudo apt purge"
